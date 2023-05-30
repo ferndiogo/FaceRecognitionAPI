e708ca690e06c5d411d81170b295c9bb6be3e048
vasco2001 <102040726+vasco2001@users.noreply.github.com>
2023-05-30 11:10:42 +0100

    Adição do model para o registro da picagem
    Co-Authored-By: Fernando Diogo <102040861+ferndiogo@users.noreply.github.com>

---
+++ FaceRecognitionAPI/FaceRecognitionAPI/FaceRecognitionAPI/Models/Registry.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FaceRecognitionAPI.Models {
+    public class Registry {
+        /// <summary>
+        /// PK do Registo
+        /// </summary>
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Regista a hora e data do registo
+        /// </summary>
+        [Required]
+        public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// Regista se o registo foi de entrada ou saida
+        /// </summary>
+        [Required]
+        public string Type { get; set; } //Entrada ou Saida
+
+        /// <summary>
+        /// FK para o funcionário
+        /// </summary>
+        [Required]
+        public int EmployeeId { get; set; }
+
+        /// <summary>
+        /// Referencia para o Funcionário
+        /// </summary>
+        public Employee Employee { get; set; }
+    }
+}
