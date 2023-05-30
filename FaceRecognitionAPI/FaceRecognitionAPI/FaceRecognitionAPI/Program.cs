8cbbe1308973403b5cb388b67ca0aa3c2ee93d5e
Fernando Diogo <aluno22111@ipt.pt>
2023-05-30 12:40:56 +0100

    Instalação de pacotes necessários para o projeto e configurações para a BD

--- FaceRecognitionAPI/FaceRecognitionAPI/FaceRecognitionAPI/Program.cs
+++ FaceRecognitionAPI/FaceRecognitionAPI/FaceRecognitionAPI/Program.cs
@@ -1,6 +1,15 @@
+using FaceRecognitionAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
