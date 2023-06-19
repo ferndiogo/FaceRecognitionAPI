935fc63d8e8af09b532a796e3247235e6f974612
vasco2001 <102040726+vasco2001@users.noreply.github.com>
2023-06-19 20:41:00 +0100

    Adição de alguns métodos para gerir os utilizadores e adição de DTO para os users

---
+++ FaceRecognitionAPI/FaceRecognitionAPI/FaceRecognitionAPI/DTO/UserDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace FaceRecognitionAPI.DTO
+{
+    public class UserDTO
+    {
+        /// <summary>
+        /// PK da tabela dos utilizadores
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome do utilizador
+        /// </summary>
+        public string Username { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Token para autenticação
+        /// </summary>
+        public DateTime TokenCreated { get; set; }
+
+        /// <summary>
+        /// Regra do utilizador
+        /// </summary>
+        public String Role { get; set; }
+    }
+}
