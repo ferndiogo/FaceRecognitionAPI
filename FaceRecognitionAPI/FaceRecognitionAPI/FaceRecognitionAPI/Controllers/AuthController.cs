bb20f9f804398bca0cc4e3320baf7d01bffd5ce3
Fernando Diogo <aluno22111@ipt.pt>
2023-07-15 19:38:15 +0100

    Correção de erros.

--- FaceRecognitionAPI/FaceRecognitionAPI/FaceRecognitionAPI/Controllers/AuthController.cs
+++ FaceRecognitionAPI/FaceRecognitionAPI/FaceRecognitionAPI/Controllers/AuthController.cs
@@ -2,7 +2,6 @@
 using FaceRecognitionAPI.DTO;
 using FaceRecognitionAPI.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -32,7 +31,7 @@ namespace FaceRecognitionAPI.Controllers
             _configuration = configuration;
             this.db = db;
         }
-        
+
         /// <summary>
         /// Registar um novo utilizador
         /// </summary>
@@ -170,171 +169,165 @@ namespace FaceRecognitionAPI.Controllers
             if (userDB == null)
             { return BadRequest("Unregistered User"); }
 
-            if (!(db.Users.Where(x => x.Username == user.Username).Any()))
-            {               
 
-                userDB.Username = user.Username;
-                userDB.Role = user.Role;
+            userDB.Username = user.Username;
+            userDB.Role = user.Role;
 
-                UserDTO dto = new UserDTO
-                {
-                    Id = userDB.Id,
-                    Username = userDB.Username,
-                    TokenCreated = userDB.TokenCreated,
-                    Role = userDB.Role
-                };
-
-                await db.SaveChangesAsync();
-
-                return Ok(dto);
-            }
-            else
+            UserDTO dto = new UserDTO
             {
-                return BadRequest("Username in use");
-            }
-        }
+                Id = userDB.Id,
+                Username = userDB.Username,
+                TokenCreated = userDB.TokenCreated,
+                Role = userDB.Role
+            };
 
-        /// <summary>
-        /// Eliminar utilizador
-        /// </summary>
-        /// <param name="Id"></param>
-        /// <returns></returns>
-        [Authorize(Roles = "Admin")]
-        [HttpDelete("delete/{Id}")]
-        public async Task<IActionResult> DeleteUser(int Id)
-        {
-            var user = await db.Users.FindAsync(Id);
-            if (user == null)
-            { return BadRequest("Unregistered employee"); }
-
-            db.Users.Remove(user);
             await db.SaveChangesAsync();
-            return Ok("User successfully removed");
+
+            return Ok(dto);
+
+    }
+
+    /// <summary>
+    /// Eliminar utilizador
+    /// </summary>
+    /// <param name="Id"></param>
+    /// <returns></returns>
+    [Authorize(Roles = "Admin")]
+    [HttpDelete("delete/{Id}")]
+    public async Task<IActionResult> DeleteUser(int Id)
+    {
+        var user = await db.Users.FindAsync(Id);
+        if (user == null)
+        { return BadRequest("Unregistered employee"); }
+
+        db.Users.Remove(user);
+        await db.SaveChangesAsync();
+        return Ok("User successfully removed");
+    }
+
+    /// <summary>
+    /// Obter o username a partir do token
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("Username")]
+    [Authorize(Roles = "Admin, User")]
+    public async Task<ActionResult<string>> GetUsername()
+    {
+        var identity = User.Identity.Name;
+        //var username = identity.Claims.FirstOrDefault(c => c.Type == "username").Value;
+
+        var user = await db.Users.FirstOrDefaultAsync(x => x.Username == identity);
+
+        if (user == null)
+        {
+            return NotFound();
         }
 
-        /// <summary>
-        /// Obter o username a partir do token
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet("Username")]
-        [Authorize(Roles = "Admin, User")]
-        public async Task<ActionResult<string>> GetUsername()
+        return Ok(user.Username);
+
+    }
+
+    /// <summary>
+    /// Obter a role do utilizador a partir do token
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("Roles")]
+    [Authorize(Roles = "Admin, User")]
+    public async Task<ActionResult<string>> GetRoles()
+    {
+        var identity = User.Identity.Name;
+        //var username = identity.Claims.FirstOrDefault(c => c.Type == "username").Value;
+
+        var user = await db.Users.FirstOrDefaultAsync(x => x.Username == identity);
+
+        if (user == null)
         {
-            var identity = User.Identity.Name;
-            //var username = identity.Claims.FirstOrDefault(c => c.Type == "username").Value;
-
-            var user = await db.Users.FirstOrDefaultAsync(x => x.Username == identity);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(user.Username);
-
+            return NotFound();
         }
 
-        /// <summary>
-        /// Obter a role do utilizador a partir do token
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet("Roles")]
-        [Authorize(Roles = "Admin, User")]
-        public async Task<ActionResult<string>> GetRoles()
+        return Ok(user.Role);
+
+    }
+
+    /// <summary>
+    /// Obter o id do utilizador a partir do token
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("Id")]
+    [Authorize(Roles = "Admin, User")]
+    public async Task<ActionResult<string>> GetId()
+    {
+        var identity = User.Identity.Name;
+        //var username = identity.Claims.FirstOrDefault(c => c.Type == "username").Value;
+
+        var user = await db.Users.FirstOrDefaultAsync(x => x.Username == identity);
+
+        if (user == null)
         {
-            var identity = User.Identity.Name;
-            //var username = identity.Claims.FirstOrDefault(c => c.Type == "username").Value;
-
-            var user = await db.Users.FirstOrDefaultAsync(x => x.Username == identity);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(user.Role);
-
+            return NotFound();
         }
 
-        /// <summary>
-        /// Obter o id do utilizador a partir do token
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet("Id")]
-        [Authorize(Roles = "Admin, User")]
-        public async Task<ActionResult<string>> GetId()
-        {
-            var identity = User.Identity.Name;
-            //var username = identity.Claims.FirstOrDefault(c => c.Type == "username").Value;
+        return Ok(user.Id);
 
-            var user = await db.Users.FirstOrDefaultAsync(x => x.Username == identity);
+    }
 
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(user.Id);
-
-        }
-
-        /// <summary>
-        /// Função para criar o token JWT para o utilizador
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
-        private string CreateToken(User user)
-        {
-            List<Claim> claims = new List<Claim>
+    /// <summary>
+    /// Função para criar o token JWT para o utilizador
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    private string CreateToken(User user)
+    {
+        List<Claim> claims = new List<Claim>
             {
                 new Claim(ClaimTypes.Name, user.Username),
                 new Claim(ClaimTypes.Role, user.Role)
             };
 
-            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
-                _configuration.GetSection("AppSettings:Token").Value));
+        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+            _configuration.GetSection("AppSettings:Token").Value));
 
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: user.TokenExpires,
-                signingCredentials: creds);
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: user.TokenExpires,
+            signingCredentials: creds);
 
-            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
-            return jwt;
-        }
+        return jwt;
+    }
 
-        /// <summary>
-        /// Criar o hash e o salt da password
-        /// </summary>
-        /// <param name="password"></param>
-        /// <param name="passwordHash"></param>
-        /// <param name="passwordSalt"></param>
-        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    /// <summary>
+    /// Criar o hash e o salt da password
+    /// </summary>
+    /// <param name="password"></param>
+    /// <param name="passwordHash"></param>
+    /// <param name="passwordSalt"></param>
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
         {
-            using (var hmac = new HMACSHA512())
-            {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
-        }
-
-        /// <summary>
-        /// Verifica se a password do utilizador corresponde ao hash armazenado
-        /// </summary>
-        /// <param name="password"></param>
-        /// <param name="passwordHash"></param>
-        /// <param name="passwordSalt"></param>
-        /// <returns></returns>
-        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
-        {
-            using (var hmac = new HMACSHA512(passwordSalt))
-            {
-                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-                return computedHash.SequenceEqual(passwordHash);
-            }
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
         }
     }
+
+    /// <summary>
+    /// Verifica se a password do utilizador corresponde ao hash armazenado
+    /// </summary>
+    /// <param name="password"></param>
+    /// <param name="passwordHash"></param>
+    /// <param name="passwordSalt"></param>
+    /// <returns></returns>
+    private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512(passwordSalt))
+        {
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(passwordHash);
+        }
+    }
+}
 }
